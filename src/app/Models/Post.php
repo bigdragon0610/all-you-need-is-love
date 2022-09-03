ece32f3de385643072e74f95110137bd45b43f0c
ryudai <bigdragon.06102001@gmail.com>
2022-09-03 17:27:25 +0900

    postからslackに通知ができる

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -4,18 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\SlackNotification;
 
 class Post extends Model
 {
-    use HasFactory;
+  use HasFactory, Notifiable;
 
-    public function user()
-    {
-        return $this->belongsTo('App\Models\User');
-    }
+  public function user()
+  {
+    return $this->belongsTo('App\Models\User');
+  }
 
-    public function channel()
-    {
-        return $this->belongsTo('App\Models\Channel');
-    }
+  public function channel()
+  {
+    return $this->belongsTo('App\Models\Channel');
+  }
+
+  public function routeNotificationForSlack()
+  {
+    return config("slack.url");
+  }
+
+  public function sendSlack($message)
+  {
+    return $this->notify(new SlackNotification($message));
+  }
 }
