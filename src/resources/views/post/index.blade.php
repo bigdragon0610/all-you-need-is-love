d7028c9863f77b68cac86044a3f4f64220a2b781
hinatakahashi <hina0414takahashi@gmail.com>
2022-09-04 18:33:36 +0900

    editアイコン挿入

--- src/resources/views/post/index.blade.php
+++ src/resources/views/post/index.blade.php
@@ -19,6 +19,12 @@
                             <img src="{{ asset('img/delete_icon.svg') }}" alt="delete btn" width="28">
                         </button>
                     </form>
+                    <form action="{{ route('post.edit', ['post' => $post->id]) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="ml-auto block mt-3">
+                            <img src="{{ asset('img/edit.svg') }}" alt="edit btn" width="28">
+                        </button>
+                    </form>
                 </div>
             </div>
         @endforeach
